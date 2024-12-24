8dbe9b6ae41e2eb65fad4dd715d70aa5c7bfd31c
Andres Hernandez <108487257+andresghg@users.noreply.github.com>
2024-12-23 21:20:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirect() {
+    // window.location.href="file:///C:/Users/Andres%20Hernandez/Downloads/Profile.pdf";
+    window.open("img/Profile2.pdf",'Download');
+    
+}
+
+function redirectD(valor) {
+
+    window.open("https://"+valor) ;
+
+}
